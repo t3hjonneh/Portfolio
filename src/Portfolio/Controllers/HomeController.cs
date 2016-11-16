69166d42ff2215e2ab0b4332649c93d4b6af6da3
ri_jonhogue <jonshogue@gmail.com>
2016-11-15 21:22:20 -0500

    Initial creation.

--- src/Portfolio/Controllers/HomeController.cs
+++ src/Portfolio/Controllers/HomeController.cs
@@ -14,16 +14,12 @@ namespace Portfolio.Controllers
         }
 
         public IActionResult About()
-        {
-            ViewData["Message"] = "Your application description page.";
-
+        {        
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
-
             return View();
         }
 
